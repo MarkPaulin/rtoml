c7b65e57a9f9893cc2ba60835fc6f2645f5a1676
Mark Paulin <mark.paulin@nisra.gov.uk>
2026-01-05 11:20:29 +0000

    make flattening optional

--- tests/testthat/test-arrays.R
+++ tests/testthat/test-arrays.R
@@ -15,3 +15,17 @@ test_that("simple arrays are flattened", {
   x <- parse_toml("x = [1, 1.0, 1]")
   expect_identical(x, list(x = list(1L, 1, 1L)))
 })
+
+test_that("flattening can be disabled", {
+  x <- parse_toml("x = [1, 2, 3]", flatten = FALSE)
+  expect_identical(x, list(x = list(1L, 2L, 3L)))
+  
+  x <- parse_toml("x = [1.0, 2.0, 3.0]", flatten = FALSE)
+  expect_identical(x, list(x = list(1, 2, 3)))
+  
+  x <- parse_toml("x = [true, false, true]", flatten = FALSE)
+  expect_identical(x, list(x = list(TRUE, FALSE, TRUE)))
+  
+  x <- parse_toml("x = ['a', 'b', 'cde']", flatten = FALSE)
+  expect_identical(x, list(x = list("a", "b", "cde")))
+})
